fda2d7dbb475ce524fa84da6177770c2cb35af91
Morix <rade1500@gmail.com>
2017-08-24 14:37:51 +0000

    fix bug build.sh

--- script/build.sh
+++ script/build.sh
@@ -10,14 +10,14 @@ theme_name=angels-ladder
 public_dir=./public
 
 if [ ! -e ${hugo_bin} ]; then
-    hugo_tar=hugo.tar.gz
-    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz -o ${hugo_dir}/${hugo_tar}
-
     mkdir ${hugo_dir} ; pushd ${hugo_dir}
+
+    hugo_tar=hugo.tar.gz
+    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz -o ${hugo_tar}
+
     tar zxvf ${hugo_tar}
     popd
 fi
 
 ${hugo_bin} -t ${theme_name}
-
 
