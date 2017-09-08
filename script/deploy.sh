b3547b5fb8d84e92b4c485dd5dd9831e841f7cf8
Morix <rade1500@gmail.com>
2017-09-08 11:29:23 +0000

    Change to firebase

--- script/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws s3 sync --delete ./public/ s3://blog.haramishio.xyz
--- script/invalidation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-distribution_id=E3M0T60TDQB914
-
-aws configure set preview.cloudfront true
-aws cloudfront create-invalidation --distribution-id ${distribution_id} --path '/*'
