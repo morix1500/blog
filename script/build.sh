945500c7fb43734c41e51d6b607d4390657f1115
Morix <rade1500@gmail.com>
2017-10-29 15:48:06 +0900

    modfy contents

--- script/build.sh
+++ script/build.sh
@@ -13,7 +13,7 @@ if [ ! -e ${hugo_bin} ]; then
     mkdir ${hugo_dir} ; pushd ${hugo_dir}
 
     hugo_tar=hugo.tar.gz
-    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.26/hugo_0.26_Linux-64bit.tar.gz -o ${hugo_tar}
+    curl -sL https://github.com/gohugoio/hugo/releases/download/v0.30.2/hugo_0.30.2_Linux-64bit.tar.gz -o ${hugo_tar}
 
     tar zxvf ${hugo_tar}
     popd
